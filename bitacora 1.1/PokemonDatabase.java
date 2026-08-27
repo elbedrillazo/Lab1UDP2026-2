7a8c86b914df6bdbe5fc5ac23d2cd4198a91beaa
CarlosGarcia-maker <carlosgarcia19072512@gmail.com>
2026-08-26 22:24:48 -0400

    Update PokemonDatabase.java

--- bitacora 1.1/PokemonDatabase.java
+++ bitacora 1.1/PokemonDatabase.java
@@ -1,46 +1,42 @@
 import java.util.ArrayList;
 
-/**
- * Gestiona una lista de objetos Pokemon y expone los métodos de ordenamiento y búsqueda[cite: 1].
+/*
+  Gestiona una lista de objetos Pokemon y expone los métodos de ordenamiento y búsqueda[cite: 1]
  */
 public class PokemonDatabase {
 
     private ArrayList<Pokemon> pokemons;
 
-    /**
-     * Constructor de la base de datos.
-     * @param pokemons Lista inicial de Pokémon.
+    /*
+     es el constructor de la base de datos
+     lista inicial de Pokémon.
      */
     public PokemonDatabase(ArrayList<Pokemon> pokemons) {
     }
 
-    /**
-     * Ordena la lista de Pokémon mediante el algoritmo y atributo especificados[cite: 1].
-     *
-     * @param algoritmo Valores aceptados: "selectionSort" o "mergeSort"[cite: 1].
-     * @param atributo  Campo para ordenar (ej. "hp", "attack", "totalStats")[cite: 1].
+    /*
+      se ordena la lista de Pokémon mediante el algoritmo y atributo especificados
+     algoritmo Valores aceptados: "selectionSort" o "mergeSort"[cite: 1]
+     atributo  Campo para ordenar (ej. "hp", "attack", "totalStats")[cite: 1]
      */
     public void ordenarPorAlgoritmo(String algoritmo, String atributo) {
     }
 
-    /**
-     * Recorre la lista desde el inicio hasta el final buscando coincidencias exactas[cite: 1].
-     *
-     * @param clave    Valor a buscar.
-     * @param atributo Atributo sobre el cual buscar.
-     * @return ArrayList con todos los Pokémon que coinciden[cite: 1].
+    /*
+     * Recorre la lista desde el inicio hasta el final buscando coincidencias exactas
+     * valor a buscar
+     * atributo sobre el cual buscar
+     * arrayList con todos los Pokémon que coinciden
      */
     public ArrayList<Pokemon> sequentialSearch(String clave, String atributo) {
         return null;
     }
 
-    /**
-     * Búsqueda binaria sobre una lista previamente ordenada[cite: 1].
-     * Su complejidad temporal esperada es O(log(n) + k)[cite: 1].
-     *
-     * @param clave    Valor a buscar.
-     * @param atributo Atributo sobre el cual buscar.
-     * @return ArrayList con todas las coincidencias contiguas[cite: 1].
+    /*
+     Búsqueda binaria sobre una lista previamente ordenada
+     clave    Valor a buscar
+     atributo sobre el cual buscar
+     arrayList con todas las coincidencias contiguas
      */
     public ArrayList<Pokemon> binarySearch(String clave, String atributo) {
         return null;
