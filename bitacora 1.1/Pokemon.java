51c480dde155b3da7d213a89eb2c3f76b62cc840
Sebastian-Mamani <sebastian.mamani@mail.udp.cl>
2026-08-26 22:44:01 -0400

    Refactor getters and setters in Pokemon class

--- bitacora 1.1/Pokemon.java
+++ bitacora 1.1/Pokemon.java
@@ -28,28 +28,44 @@ public class Pokemon {
     public Pokemon(int id, String name, String type1, int hp, int attack, int defense, int speed) {
     }
 
-    public int getId() { return 0; }
-    public void setId(int id) { }
-
-    public String getName() { return null; }
-    public void setName(String name) { }
-
-    public String getType1() { return null; }
-    public void setType1(String type1) { }
-
-    public int getHp() { return 0; }
-    public void setHp(int hp) { }
-
-    public int getAttack() { return 0; }
-    public void setAttack(int attack) { }
-
-    public int getDefense() { return 0; }
-    public void setDefense(int defense) { }
-
-    public int getSpeed() { return 0; }
-    public void setSpeed(int speed) { }
-
-    public int getTotalStats() { return 0; }
+    public int getId() { 
+     return 0;
+    }
+    public void setId(int id) { 
+    }
+    public String getName() { 
+     return null;
+    }
+    public void setName(String name) { 
+    }
+    public String getType1() { 
+     return null;
+    }
+    public void setType1(String type1) { 
+    }
+    public int getHp() { 
+     return 0;
+    }
+    public void setHp(int hp) { 
+    }
+    public int getAttack() {
+     return 0;
+    }
+    public void setAttack(int attack) { 
+    }
+    public int getDefense() { 
+     return 0;
+    }
+    public void setDefense(int defense) { 
+    }
+    public int getSpeed() { 
+     return 0;
+    }
+    public void setSpeed(int speed) { 
+    }
+    public int getTotalStats() { 
+     return 0;
+    }
     public void setTotalStats(int totalStats) { }
 
     public static final Comparator<Pokemon> BY_NAME = null;
