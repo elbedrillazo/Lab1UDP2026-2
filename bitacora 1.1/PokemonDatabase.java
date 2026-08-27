604284effbe66dc2c480eb6104e585ceea13d3f7
CarlosGarcia-maker <carlosgarcia19072512@gmail.com>
2026-08-26 22:35:20 -0400

    Update PokemonDatabase.java

--- bitacora 1.1/PokemonDatabase.java
+++ bitacora 1.1/PokemonDatabase.java
@@ -1,14 +1,10 @@
 import java.util.ArrayList;
-
-/*
-  Gestiona una lista de objetos Pokemon y expone los métodos de ordenamiento y búsqueda[cite: 1]
- */
 public class PokemonDatabase {
 
     private ArrayList<Pokemon> pokemons;
 
     /*
-     es el constructor de la base de datos
+     es el constructor de la base de datos y la 
      lista inicial de Pokémon.
      */
     public PokemonDatabase(ArrayList<Pokemon> pokemons) {
@@ -16,17 +12,12 @@ public class PokemonDatabase {
 
     /*
       se ordena la lista de Pokémon mediante el algoritmo y atributo especificados
-     algoritmo Valores aceptados: "selectionSort" o "mergeSort"[cite: 1]
-     atributo  Campo para ordenar (ej. "hp", "attack", "totalStats")[cite: 1]
      */
     public void ordenarPorAlgoritmo(String algoritmo, String atributo) {
     }
 
     /*
-     * Recorre la lista desde el inicio hasta el final buscando coincidencias exactas
-     * valor a buscar
-     * atributo sobre el cual buscar
-     * arrayList con todos los Pokémon que coinciden
+     Recorre la lista desde el inicio hasta el final buscando coincidencias exactas
      */
     public ArrayList<Pokemon> sequentialSearch(String clave, String atributo) {
         return null;
@@ -34,9 +25,6 @@ public class PokemonDatabase {
 
     /*
      Búsqueda binaria sobre una lista previamente ordenada
-     clave    Valor a buscar
-     atributo sobre el cual buscar
-     arrayList con todas las coincidencias contiguas
      */
     public ArrayList<Pokemon> binarySearch(String clave, String atributo) {
         return null;
