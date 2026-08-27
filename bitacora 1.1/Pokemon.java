abee813a9e85987a498de8a7634ce2293e0e7785
Bedrill <64346927+elbedrillazo@users.noreply.github.com>
2026-08-26 22:53:26 -0400

    Update Pokemon.java

--- bitacora 1.1/Pokemon.java
+++ bitacora 1.1/Pokemon.java
@@ -1,10 +1,8 @@
 import java.util.Comparator;
 
-/*
- Representa un Pokemon dentro de la base de datos con sus estadísticas de combate
- */
+/* representa un Pokemon dentro de la base de datos con sus estadísticas de combate */
 public class Pokemon {
-    
+ 
     private int id;
     private String name;
     private String type1;
@@ -14,52 +12,67 @@ public class Pokemon {
     private int speed;
     private int totalStats;
 
-    /**
-      Constructor de la clase Pokemon
-      Recibe la ID, nombre, tipo principal, y las estadisticas del pokemon (vida, ataque, defensa, velocidad).
-     */
+    /* constructor de la clase Pokemon y recibe la ID, nombre, tipo principal, y las estadisticas del pokemon (vida, ataque, defensa, velocidad) */
+ 
     public Pokemon(int id, String name, String type1, int hp, int attack, int defense, int speed) {
+     
     }
-
     public int getId() { 
+     
      return 0;
     }
     public void setId(int id) { 
+     
     }
     public String getName() { 
+     
      return null;
     }
     public void setName(String name) { 
+     
     }
     public String getType1() { 
+     
      return null;
     }
     public void setType1(String type1) { 
+     
     }
     public int getHp() { 
+     
      return 0;
     }
     public void setHp(int hp) { 
+     
     }
     public int getAttack() {
+     
      return 0;
     }
     public void setAttack(int attack) { 
+     
     }
     public int getDefense() { 
+     
      return 0;
     }
     public void setDefense(int defense) { 
+     
     }
     public int getSpeed() { 
+     
      return 0;
     }
     public void setSpeed(int speed) { 
+     
     }
     public int getTotalStats() { 
+     
      return 0;
     }
-    public void setTotalStats(int totalStats) { }
+    public void setTotalStats(int totalStats) {
+     
+    }
 
     public static final Comparator<Pokemon> BY_NAME = null;
     public static final Comparator<Pokemon> BY_HP = null;
