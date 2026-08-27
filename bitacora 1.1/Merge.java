e383df1b1f06150ec8e904dc7fcfbe09ff851107
Bedrill <64346927+elbedrillazo@users.noreply.github.com>
2026-08-26 22:19:57 -0400

    Update Merge.java

--- bitacora 1.1/Merge.java
+++ bitacora 1.1/Merge.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 
-/** aqui se aplicara el merge adaptado para el arraylist de pokemon */
+/* aqui se aplicara el merge adaptado para el arraylist de pokemon */
 public class Merge {
     
     private Merge() { }
     
-    /**organiza en orden ascendente con el comparator recibido para la lista de pokemon a ordenar
+    /* organiza en orden ascendente con el comparator recibido para la lista de pokemon a ordenar
     y el comparator deberia definir el criterio */
     
     public static void sort(ArrayList<Pokemon> a, Comparator<Pokemon> comparator) {
