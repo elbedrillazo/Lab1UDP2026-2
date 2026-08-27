89f365938145abf76185afccb9fa97345c3feb04
Bedrill <64346927+elbedrillazo@users.noreply.github.com>
2026-08-26 22:11:12 -0400

    Create PokemonGenerator.java

---
+++ bitacora 1.1/PokemonGenerator.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * Clase responsable de generar las bases de datos sintéticas de Pokémon[cite: 1].
+ */
+public class PokemonGenerator {
+
+    /**
+     * Genera una base de datos sintética garantizando la reproducibilidad mediante una semilla[cite: 1].
+     *
+     * @param n    Tamaño de la instancia (potencia de 2)[cite: 1].
+     * @param seed Semilla para la generación de números aleatorios[cite: 1].
+     * @return ArrayList con n objetos Pokemon[cite: 1].
+     */
+    public static ArrayList<Pokemon> generateDatabase(int n, long seed) {
+        return null;
+    }
+}
