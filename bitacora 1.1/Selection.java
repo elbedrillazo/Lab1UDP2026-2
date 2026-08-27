a65b69ed1ff078d31b137e97b068c424afd1c51c
Bedrill <64346927+elbedrillazo@users.noreply.github.com>
2026-08-26 22:55:33 -0400

    Update Selection.java

--- bitacora 1.1/Selection.java
+++ bitacora 1.1/Selection.java
@@ -4,10 +4,13 @@ import java.util.Comparator;
 /* selection adaptado para el arraylist */
 public class Selection {
     
-    private Selection() { }
+    private Selection() {
+        
+    }
     
     /* organiza de forma ascendente con el comparator */
     
     public static void sort(ArrayList<Pokemon> a, Comparator<Pokemon> comparator) {
+        
     }
 }
