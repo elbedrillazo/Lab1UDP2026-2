4b8c47e54123f93fe761fccfff56de71b8435a4c
Bedrill <64346927+elbedrillazo@users.noreply.github.com>
2026-09-02 13:49:36 -0400

    Create Pokemon.java

---
+++ bitacora 1.2/Pokemon.java
@@ -0,0 +1,52 @@
+import java.util.Comparator;
+
+public class Pokemon {
+    private int id;
+    private String name;
+    private String type1;
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
+    private int totalStats;
+
+    public Pokemon(int id, String name, String type1, int hp, int attack, int defense, int speed) {
+        this.id = id;
+        this.name = name;
+        this.type1 = type1;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.totalStats = hp + attack + defense + speed; 
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getType1() { return type1; }
+    
+    public int getHp() { return hp; }
+    public void setHp(int hp) { this.hp = hp; actualizarTotalStats(); }
+
+    public int getAttack() { return attack; }
+    public void setAttack(int attack) { this.attack = attack; actualizarTotalStats(); }
+
+    public int getDefense() { return defense; }
+    public void setDefense(int defense) { this.defense = defense; actualizarTotalStats(); }
+
+    public int getSpeed() { return speed; }
+    public void setSpeed(int speed) { this.speed = speed; actualizarTotalStats(); }
+
+    public int getTotalStats() { return totalStats; }
+
+    private void actualizarTotalStats() {
+        this.totalStats = this.hp + this.attack + this.defense + this.speed;
+    }
+
+    public static final Comparator<Pokemon> BY_NAME = Comparator.comparing(Pokemon::getName);
+    public static final Comparator<Pokemon> BY_HP = Comparator.comparingInt(Pokemon::getHp);
+    public static final Comparator<Pokemon> BY_ATTACK = Comparator.comparingInt(Pokemon::getAttack);
+    public static final Comparator<Pokemon> BY_DEFENSE = Comparator.comparingInt(Pokemon::getDefense);
+    public static final Comparator<Pokemon> BY_SPEED = Comparator.comparingInt(Pokemon::getSpeed);
+    public static final Comparator<Pokemon> BY_TOTAL_STATS = Comparator.comparingInt(Pokemon::getTotalStats);
+}
