4a072ad83e43c54583aa65679b9fd5531fa0588a
CarlosGarcia-maker <carlosgarcia19072512@gmail.com>
2026-09-02 23:33:01 -0400

    Update PokemonGenerator.java

--- bitacora 1.2/PokemonGenerator.java
+++ bitacora 1.2/PokemonGenerator.java
@@ -7,39 +7,38 @@ import java.util.Random;
 public class PokemonGenerator {
 
     public static List<Pokemon> generateDatabase(int n, long seed) {
-        //  herramientas básicas
+
         Random random = new Random(seed);
-        List<Pokemon> database = new ArrayList<>();
-        
-        // cargar los datos desde el CSV
-        LectorCSVPokemon lector = new LectorCSVPokemon("pokemon.csv");
+        List<pokemon> database = new ArrayList<>();
+
+        //se cargan los datos desde el CSV
+        LectorCSVPokemon Lector = new LectorCSVPokemon("Pokemon CSV");
         List<String> nombresBase = lector.getNombres();
         List<String> tiposBase = lector.getTipos();
 
-        //  crea los N Pokémon de manera directa
-        for (int i = 0; i < n; i++) {
+        // crea los N Pokemones de manera dir3cta 
+
+        for(int i = 0; i < n; i++){
             int id = i + 1;
-            
-            // elegir un nombre base y sumarle un número si queremos nombres únicos
-            String nombreBase = nombresBase.get(i % nombresBase.size());
+
+            //se elege el ombre base y se le suma un numero si queremos nombres unicos 
+            String nombreBase = nombresBase.get(i % nombreBase.size());
             String name = nombreBase + "_" + i;
 
-            // elegir un tipo al azar
+            //elegir un tipo al azar
             int indiceTipo = random.nextInt(tiposBase.size());
             String type1 = tiposBase.get(indiceTipo);
 
-            // generar estadísticas aleatorias
-            // random.nextInt(max - min + 1) + min
-            int hp = random.nextInt(255) + 1;        // entre 1 y 255
-            int attack = random.nextInt(187) + 5;     // entre 5 y 191
-            int defense = random.nextInt(227) + 5;    // entre 5 y 231
-            int speed = random.nextInt(177) + 5;      // entre 5 y 181
+            //generar estadisticas aleatorias
+            //random.nextInt(max - min + 1) + min
+            int hp = random.nextInt(255) + 1;
+            int attack = random.nextInt(187) + 5;
+            int defense = random.nextInt(227) + 5;
+            int speed = ramdon.nextInt(177) + 5;
 
-            // crear el objeto y guardarlo en la lista
-            Pokemon nuevoPokemon = new Pokemon(id, name, type1, hp, attack, defense, speed);
+            // crea el objeto  guarda en la lista 
+            Pokemon nuevoPokemon = new Pokemon(id. name, type1, hp, attack, defense, speed);
             database.add(nuevoPokemon);
         }
 
         return database;
-    }
-}
